17a8781c94fadb63956228a4c81948e5122a5901
JUAN <jvelandia1120@gmail.com>
2026-03-06 07:27:55 -0500

    segundo avanze

--- sistema.py
+++ sistema.py
@@ -1,32 +1,43 @@
+
+# 1. IDENTIFICACIÓN Y CONFIGURACIÓN INICIAL
 # 1. IDENTIFICACIÓN
 # Estudiante: [Tu Nombre Aquí]
 # Proyecto: RIVENT (Sistema de facturación e inventario)
-# 2. VARIABLES INICIALES
-nombre_local = "POLLO Y CHARCUTERIA RAUL"
-nombre_producto = "SALCHICHAS ZENU"
-precio_venta = 45000.0  # float
-cant_vendida = 3        # int
-porcentaje_Iva = 0.19   # float
+
+print("-------------------------------------------------")
+print(" Bienvenido al sistema de facturación RIVENT ")
+print("-------------------------------------------------\n")
+
+# 2. ENTRADA DE DATOS (INTERACTIVIDAD)
+# Usamos input() para preguntar y float()/int() para convertir ese texto en números.
+nombre_local = input("Ingresa el nombre del local: ")
+nombre_producto = input("Ingresa el nombre del producto: ")
+precio_venta = float(input("Ingresa el precio por unidad (Ej. 45000): "))
+cant_vendida = int(input("Ingresa la cantidad que lleva el cliente: "))
+porcentaje_Iva = 0.19   # Mantenemos el IVA fijo en 19%
+
 # 3. OPERACIONES BÁSICAS
 # Saco el subtotal multiplicando el precio por lo que compraron
 subtotal = precio_venta * cant_vendida
+
 # Calculo de cuánto es el IVA de esa compra
 valor_iva = subtotal * porcentaje_Iva
+
 # Sumo todo para saber cuánto tiene que pagar el cliente
 total_pagar = subtotal + valor_iva
-# 4. SALUDO Y RESULTADOS
-print("-------------------------------------------------")
-print(" Bienvenido al sistema de facturación RIVENT ")
-print(" Local:", nombre_local)
-print("-------------------------------------------------")
-print("Producto:", nombre_producto)
-print("Cantidad que lleva:", cant_vendida)
-print("Precio unidad: $", precio_venta)
+
+# 4. RESULTADOS (TICKET DE COMPRA)
+# Usamos la letra 'f' antes de las comillas para poder inyectar las variables directamente con {}
+print("\n=================================================")
+print(f" FACTURA - {nombre_local.upper()} ")
+print("=================================================")
+print(f"Producto:           {nombre_producto.upper()}")
+print(f"Cantidad:           {cant_vendida}")
+print(f"Precio unidad:      ${precio_venta:,.0f}")
 print(".................................................")
-print("Subtotal: $", subtotal)
-print("IVA (19%): $", valor_iva)
-print("TOTAL A COBRAR: $", total_pagar)
-print("-------------------------------------------------")
-
-
-
+print(f"Subtotal:           ${subtotal:,.0f}")
+print(f"IVA (19%):          ${valor_iva:,.0f}")
+print(f"TOTAL A COBRAR:     ${total_pagar:,.0f}")
+print("=================================================")
+print(" ¡Gracias por su compra! ")
+print("=================================================")
